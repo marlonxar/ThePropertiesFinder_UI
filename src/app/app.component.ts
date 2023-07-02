73e9c0b07669a5530a304e509454076d222166d9
Marlon Alvarez Rodriguez <58054501+marlonxar@users.noreply.github.com>
2023-07-02 01:07:05 -0600

    Loader was added (#6)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'ThePropertiesFinder.com';
+  showLoader = true;
+  loadingTime = 1000;
+
+  ngOnInit() {
+  setTimeout(() => {
+    this.showLoader = false;
+  }, this.loadingTime);
+}
 }
