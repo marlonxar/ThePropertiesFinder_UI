4a584c4185743bfc807fc99d7eaf4f73f09a713b
marlonxar <xar.marlon@gmail.com>
2023-07-02 16:04:59 -0600

    i18n translations was added

--- src/app/components/app-loader/app-loader.component.ts
+++ src/app/components/app-loader/app-loader.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-loader',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app-loader.component.scss']
 })
 export class AppLoaderComponent {
+  lang: String;
+
+  constructor(private translateService: TranslateService){
+    this.lang = localStorage.getItem('lang') || 'en';
+  }
 
 }
--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit{
+  lang: String;
+  yourEmailKey: string = 'Your_Email';
+
+constructor(private translateService: TranslateService){
+  this.lang = localStorage.getItem('lang') || 'en';
+}
+ngOnInit(): void {
+  
+  this.translateService.get('Your_Email').subscribe((translation: string) => {
+    this.yourEmailKey = translation;});
+
+}
+
+changeLang(target: EventTarget | null) {
+  if (target instanceof HTMLSelectElement) {
+    const lang = target.value;
+   localStorage.setItem('lang', lang);
+   window.location.reload();
+  }
+}
 
 }
