41c9420e2ea3c84a02f2f0f7ed2a36403db7b5b8
marlonxar <xar.marlon@gmail.com>
2023-07-02 01:06:49 -0600

    Loader was added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { AppLoaderComponent } from './components/app-loader/app-loader.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    LandingPageComponent
+    LandingPageComponent,
+    AppLoaderComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/app-loader/app-loader.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loader',
+  templateUrl: './app-loader.component.html',
+  styleUrls: ['./app-loader.component.scss']
+})
+export class AppLoaderComponent {
+
+}
