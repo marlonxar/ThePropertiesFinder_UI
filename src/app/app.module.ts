c635b5d6242b0df468ce27d6c723c94b0ab54dad
marlonxar <xar.marlon@gmail.com>
2023-07-02 19:02:17 -0600

    Routing system was added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HeaderComponent } from './components/header/header.component';
+import { HeaderComponent } from './shared/components/header/header.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FooterComponent } from './components/footer/footer.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
-import { AppLoaderComponent } from './components/app-loader/app-loader.component';
+import { AppLoaderComponent } from './shared/components/app-loader/app-loader.component';
 import {TranslateModule, TranslateLoader} from '@ngx-translate/core';
 import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 import {HttpClientModule, HttpClient} from '@angular/common/http';
+import { AboutUsModule } from './about-us/about-us.module';
+import { BlogModule } from './blog/blog.module';
+import { ContactModule } from './contact/contact.module';
+import { FindAgentModule } from './find-agent/find-agent.module';
+import { ServicesModule } from './services/services.module';
 
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
@@ -26,6 +31,11 @@ export function HttpLoaderFactory(http: HttpClient) {
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
+    BlogModule,
+    ServicesModule,
+    ContactModule,
+    AboutUsModule,
+    FindAgentModule,
     TranslateModule.forRoot({
       loader: {
         provide: TranslateLoader,
