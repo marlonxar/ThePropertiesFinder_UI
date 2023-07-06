83cb87d74e9acc35cd5f7101ae1dfe6c207e13e1
Marlon Alvarez Rodriguez <58054501+marlonxar@users.noreply.github.com>
2023-07-06 00:42:13 -0600

    The template pages was added (#12)

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { trigger, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-blog',
   templateUrl: './blog.component.html',
-  styleUrls: ['./blog.component.scss']
+  styleUrls: ['./blog.component.scss'],
+  animations: [
+    trigger('slideFromTop', [
+      transition(':enter', [
+        style({ transform: 'translateY(-100%)', opacity: 0 }),
+        animate('500ms ease-out', style({ transform: 'translateY(0)', opacity: 1 }))
+      ])
+    ])
+  ]
 })
 export class BlogComponent {
 
