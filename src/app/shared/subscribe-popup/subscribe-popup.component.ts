37d9972d0b8d541a63b93e2ad5e06b74a3eb0ce8
marlonxar <xar.marlon@gmail.com>
2023-07-06 23:32:00 -0600

    PopUp design and translations was added

--- src/app/shared/subscribe-popup/subscribe-popup.component.ts
+++ src/app/shared/subscribe-popup/subscribe-popup.component.ts
@@ -1,20 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-subscribe-popup',
   templateUrl: './subscribe-popup.component.html',
   styleUrls: ['./subscribe-popup.component.scss']
 })
-export class SubscribePopupComponent {
+export class SubscribePopupComponent implements OnInit{
   showPopup = false;
+  lang: String;
+  yourEmailKey: string = 'Your_Email';
+
+  constructor(private translateService: TranslateService){
+    this.lang = localStorage.getItem('lang') || 'en';
+  }
 
   closePopup() {
     this.showPopup = false;
   }
 
   ngOnInit() {
+    this.translateService.get('Your_Email').subscribe((translation: string) => {
+      this.yourEmailKey = translation;});
+
   setTimeout(() => {
-    this.showPopup = true; // Mostrar el componente después de 5 segundos
+    this.showPopup = true; 
   }, 5000);
 }
 
