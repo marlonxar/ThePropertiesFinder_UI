d9a2eb76033801200a361815d9946ac0e6cb2090
marlonxar <xar.marlon@gmail.com>
2023-07-24 22:03:24 -0600

    SSO added with Google as IDP

---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    GOOGLE_SSO_CLIENT_ID: '758223229940-pr9i3m7ceke8e02mrdn60h47babgki1i.apps.googleusercontent.com',
+    GOOGLE_SSO_CLIENT_SECRET: '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'
+}
