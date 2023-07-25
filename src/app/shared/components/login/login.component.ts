2caca8b12ecdc300ec69c249f2d813a2dd9ddc52
Marlon Alvarez Rodriguez <58054501+marlonxar@users.noreply.github.com>
2023-07-24 22:04:23 -0600

    SSO added with Google as IDP (#22)

--- src/app/shared/components/login/login.component.ts
+++ src/app/shared/components/login/login.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { trigger, style, animate, transition } from '@angular/animations';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { environment } from 'src/environments/environment';
+
+declare var google: any;
 
 @Component({
   selector: 'app-login',
@@ -38,5 +41,29 @@ export class LoginComponent implements OnInit{
       console.log(this.loginForm.value);
     }
   }
+
+  ngAfterViewInit(): void {
+    google.accounts.id.initialize({
+      client_id: environment.GOOGLE_SSO_CLIENT_ID,
+      callback: (response: any) => this.handleGoogleSignIn(response)
+    });
+    google.accounts.id.renderButton(
+      document.getElementById("buttonDiv"),
+      { size: "large", theme: "outline" }  
+    );
+  }
+
+  handleGoogleSignIn(response: any) {
+    console.log(response.credential);
+
+    // This next is for decoding the idToken to an object if you want to see the details.
+    let base64Url = response.credential.split('.')[1];
+    let base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    let jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
+      return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+    console.log(JSON.parse(jsonPayload));
+  }
+
 
 }
